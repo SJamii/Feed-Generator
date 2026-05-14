c1fc3ed01c9198faf4b3c6f6e850c4f86af49480
SJamii <shafayatiuc@gmail.com>
2026-05-14 22:25:25 +0600

    callable certificate added

--- src/main/java/com/solvians/showcase/App.java
+++ src/main/java/com/solvians/showcase/App.java
@@ -1,5 +1,12 @@
 package com.solvians.showcase;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 /**
  * Hello world!
  */
@@ -8,21 +15,35 @@ public class App {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         if (args.length < 2) {
             throw new RuntimeException(
-                    "Expect at least number of threads and number of quotes."
+                    "Expect number of threads and quotes"
             );
         }
 
         int threads = Integer.parseInt(args[0]);
         int quotes = Integer.parseInt(args[1]);
 
-        CertificateUpdateGenerator generator =
-                new CertificateUpdateGenerator(threads, quotes);
+        ExecutorService executor =
+                Executors.newFixedThreadPool(threads);
 
-        generator
-                .generateQuotes()
-                .forEach(System.out::println);
+        List<Future<String>> futures =
+                new ArrayList<>();
+
+        for (int i = 0; i < quotes; i++) {
+
+            futures.add(
+                    executor.submit(
+                            new CertificateUpdateTask()
+                    )
+            );
+        }
+
+        for (Future<String> future : futures) {
+            System.out.println(future.get());
+        }
+
+        executor.shutdown();
     }
 }
